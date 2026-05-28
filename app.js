89fe4d3fa4929c420f0f24545a05d0061f4c5e1c
JH <primextras32@gmail.com>
2026-05-28 11:41:53 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-const APP_VERSION = "v1.30.21";
+const APP_VERSION = "v1.30.22";
 
 // Stamp version into title bar, app header, and schema docs heading
 document.title = document.title.replace(/v[\d.]+$/, APP_VERSION);
